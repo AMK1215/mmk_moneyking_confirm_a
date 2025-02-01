d441a0236c740e1aeef273346dc6593f96fdc24c
wlktech <stephensuu03@gmail.com>
2025-02-01 10:49:45 +0700

    logo changed

--- resources/views/user/layouts/head.blade.php
+++ resources/views/user/layouts/head.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Lucky Million</title>
-    <link rel="icon" href="{{ asset('/assets/img/l_logo.jpg') }}">
+    <title>Money King 7</title>
+    <link rel="icon" href="{{ asset('/assets/img/logo.png') }}">
     <link rel="stylesheet" href="{{ asset('slot_app/css/style.css') }}" />
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
